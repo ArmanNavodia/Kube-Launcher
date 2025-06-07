d0b8666247e320443ccb669e2ad7efffa88ab80c
ArmanNavodia <armannavodia2002@gmail.com>
2025-06-07 06:33:10 +0530

    added vpc and eks

---
+++ infra/eks.tf
@@ -0,0 +1,34 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+depends_on = [ module.vpc ]
+  cluster_name    = "eks-cluster"
+  cluster_version = "1.31"
+
+  # EKS Addons
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
+
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+  create_kms_key = false
+  attach_cluster_encryption_policy = false
+  enable_kms_key_rotation=false
+  kms_key_enable_default_policy=false
+cluster_encryption_config = []
+  eks_managed_node_groups = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2_x86_64"
+      instance_types = ["t2.micro"]
+
+      min_size = 1
+      max_size = 2
+      desired_size = 1
+    }
+  }
+}
